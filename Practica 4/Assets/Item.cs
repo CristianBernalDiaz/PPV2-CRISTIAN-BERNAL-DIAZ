14b5de090d5c3ff51ea52ed1c11f95c0e61f946d
CristianBernalDiaz <144072011+CristianBernalDiaz@users.noreply.github.com>
2023-09-18 23:44:27 -0600

    Practica 4

---
+++ Practica 4/Assets/Item.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    
+    void Start()
+    {
+        VillagerBase Cris = new VillagerBase();
+        Cris.GetName();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
